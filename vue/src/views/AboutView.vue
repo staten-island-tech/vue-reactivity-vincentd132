56d2bee748c50d46c4985c55e23c17c9552bf2ef
vincentd132 <vincentd103@nycstudents.net>
2025-01-31 14:01:22 -0500

    e

--- vue/src/views/AboutView.vue
+++ vue/src/views/AboutView.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div id="app">
+    <h1>ShopRite</h1>
+<div class="site">
+<h2>Products</h2>
+ <div class="product" v-for="(product,index)"></div>
+
+
+
+
+
+
+</div>
+</div>
 </template>
 
 <style>
