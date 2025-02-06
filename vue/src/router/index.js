6e2ea44d1d532f6993f7fe0ad342ba8582000fff
vincentd132 <vincentd103@nycstudents.net>
2025-02-06 13:59:41 -0500

    w

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,18 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import ProductList from '../views/ProductList.vue' // Import the ProductList component
-
+import ProductList from '../views/ProductList.vue'
+import App from '../views/Parent.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: ProductList, // Map '/' path to the ProductList component
+      component: ProductList,
     },
     {
       path: '/about',
       name: 'about',
-      component: () => import('../views/AboutView.vue'), // Lazy-loaded AboutView component
+      component: App,
     },
   ],
 })
